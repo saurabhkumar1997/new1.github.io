71b6630a1d5f2362dfc320e442120eafc806ded1
saurabhkumar1997 <110714677+saurabhkumar1997@users.noreply.github.com>
2022-12-24 13:05:59 +0530

    Add files via upload

---
+++ Test1.js
@@ -0,0 +1,15 @@
+/// <reference types ="cypress"/>
+describe('My first suite', function() 
+{
+it ('My first test case',function()
+
+{
+//Test Steps
+cy.visit('https://testers-hub.com/about-us/');
+
+
+})
+
+
+
+})
